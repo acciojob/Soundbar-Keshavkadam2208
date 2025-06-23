c91d9edb65e6e991a377f627f307cb4f8ecac029
Keshavkadam2208 <131426561+Keshavkadam2208@users.noreply.github.com>
2025-06-23 19:10:07 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,25 @@
 //your JS code here. If required.
+const sounds = {'applause','boo', 'gasp', 'tada', 'victory', 'wrong', 'stop'};
+const stopAllsounds = () => {
+	sounds.forEach(sound =>{
+		const audio = document.getElementById(sound);
+		audio.pause();
+		audio.currentTime() = 0;
+	});
+});
+
+const sounds.forEach(sound =>{
+	const audio = document.getElementById('audio');
+	audio.src = 'sounds/${sound}.mp3';
+	audio.id = sound;
+	document.body.appendChild(audio);
+});
+document.querySelectorAll('.btn').forEach(button=>{
+	button.addEventListener('click',() =>{
+		stopAllsounds();
+		const sound = button.innerText;
+		document.getElementById(sound).play();
+	});
+});
+document.querySelector('.stop').addEventListener('click', stopAllsounds);
+
